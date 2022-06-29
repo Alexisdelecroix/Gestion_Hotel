8d37af79f5e8ac9f5fbc7dda4b8bc3d3d8638d85
59013-36-15 <d-alexis@hotmail.fr>
2022-06-29 11:22:34 +0200

    V1 Finie

--- Gestion_Hotel/src/Main.java
+++ Gestion_Hotel/src/Main.java
@@ -1,12 +1,11 @@
-
-
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String args[]) {
 
         // déclarer et initialiser un tableau 2D
-        int[][] names = {
+        int[][] TabPrix = {
                 {100, 120, 130, 150, 0, 0},
                 {130, 160, 170, 190, 0, 0},
                 {170, 200, 210 ,230 ,270 , 350},
@@ -14,18 +13,64 @@ public class Main {
         };
 
         // afficher le tableau 2D
-        for (int[] tab: names) {
+        for (int[] tab: TabPrix) {
             for (int s: tab) {
                 System.out.print(s + "\t");
             }
             System.out.println("\n");
         }
 
+//        int j, i = 1;
+//        for (int[] tab1D : TabPrix) {
+//            j = 1;
+//            for (int val : tab1D) {
+//                System.out.println("[" + i + "][" + j + "] = " + val);
+//                j++;
+//            }
+//            i++;
+//        }
+
+        String[] TabDescript = {
+                "Lavabo", "WC, Télévision", "Cabine Douche, Télévision", "WC , cabine douche, télévision" ,
+                "wc, salle de bain, douche, télévision", "2 pièces, salle de bain, douche, wc, télévision"
+
+        };
+
+            int nbPerson;
+            int categorie;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("A combien êtes-vous ? 4 pers. max.");
+        nbPerson = sc.nextInt();
+
+        while (nbPerson > 4 || nbPerson < 1) {
+            System.out.println("Apprend a lire, 4 pers. max. et 1 pers. minimum");
+            System.out.println("A combien êtes-vous ? 4 pers. max.");
+            nbPerson = sc.nextInt();
+        }
+
+        System.out.println("Quelle catégorie de chambre voulez-vous ?");
+        categorie = sc.nextInt();
+
+        while (categorie > 6 || categorie < 1) {
+            System.out.println("Cette catégorie n'existe pas");
+            System.out.println("Quelle catégorie de chambre voulez-vous ?");
+            categorie = sc.nextInt();
+        }
+
         int j, i = 1;
-        for (int[] tab1D : names) {
+        for (int[] tab1D : TabPrix) {
             j = 1;
             for (int val : tab1D) {
-                System.out.println("[" + i + "][" + j + "] = " + val);
+                if ( i == nbPerson && j == categorie) {
+                    if (val != 0){
+                        System.out.println("Une chambre est disponible");
+                        System.out.println("Cette chambre côute "+ val+ " euros");
+                        System.out.println("Cette chambre possède : "+TabDescript[j-1]);
+                    } else {
+                        System.out.println("Cette chambre n'est pas disponible !!");
+                    }
+                }
                 j++;
             }
             i++;
