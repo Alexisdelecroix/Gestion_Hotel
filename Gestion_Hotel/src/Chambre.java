7ebea252da5b09d0b741e8385483eb7c0ece0ac2
59013-36-15 <d-alexis@hotmail.fr>
2022-06-29 15:17:42 +0200

    Project fini

--- Gestion_Hotel/src/Chambre.java
+++ Gestion_Hotel/src/Chambre.java
@@ -1,22 +1,22 @@
 import java.util.Scanner;
 
-public class Chambre extends Hotel{
+public class Chambre{
     private int numero;
     private int categorie;
     private int nbPersonnes;
 
     Scanner sc = new Scanner(System.in);
-    int[][] TabPrix = {
-            {100, 120, 130, 150, 0, 0},
-            {130, 160, 170, 190, 0, 0},
-            {170, 200, 210, 230, 270, 350},
-            {200, 230, 240, 260, 300, 400},
-    };
+//    int[][] TabPrix = {
+//            {100, 120, 130, 150, 0, 0},
+//            {130, 160, 170, 190, 0, 0},
+//            {170, 200, 210, 230, 270, 350},
+//            {200, 230, 240, 260, 300, 400},
+//    };
 
-    String[] TabDescript = {
-            "Lavabo", "WC, Télévision", "Cabine Douche, Télévision", "WC , cabine douche, télévision" ,
-            "wc, salle de bain, douche, télévision", "2 pièces, salle de bain, douche, wc, télévision"
-    };
+//    String[] TabDescript = {
+//            "Lavabo", "WC, Télévision", "Cabine Douche, Télévision", "WC , cabine douche, télévision" ,
+//            "wc, salle de bain, douche, télévision", "2 pièces, salle de bain, douche, wc, télévision"
+//    };
 
     public Chambre(int numero, int categorie, int nbPersonnes) {
         this.numero = numero;
@@ -24,78 +24,25 @@ public class Chambre extends Hotel{
         this.nbPersonnes = nbPersonnes;
     }
 
-    public int getNumero() {
+
+    public int donnerNumero() {
         return numero;
     }
-
-    public void setNumero(int numero) {
-        this.numero = numero;
-    }
-
-    public int getCategorie() {
-        return categorie;
-    }
-
-    public void setCategorie(int categorie) {
-        this.categorie = categorie;
-    }
-
-    public int getNbPersonnes() {
+     public int DonnerCateg() {
+         return categorie;
+     }
+    public int DonnerNbPers() {
         return nbPersonnes;
     }
 
-    public void setNbPersonnes(int nbPersonnes) {
-        this.nbPersonnes = nbPersonnes;
-    }
 
-    public void donnerNumero() {
-        System.out.println("Votre numéro de chambre est le " + numero);
-    }
-     public void DonnerCateg() {
-         System.out.println("Votre catégories de chambre est : "+categorie);
-     }
-    public void DonnerNbPers() {
-        System.out.println("Vous êtes "+nbPersonnes+ " personnes dans la chambre");
-    }
+    public int DonnerPrix(int tabPrix[][]){
+        return ( tabPrix[nbPersonnes-1][categorie-1] );
+    } ;
 
-
-        public void DonnerPrix() {
-            System.out.println("Quelle catégorie de chambre voulez-vous ?");
-            categorie = sc.nextInt();
-            System.out.println("A combien êtes-vous ? 4 pers. max.");
-            nbPersonnes = sc.nextInt();
-            int j, i = 1;
-            for (int[] tab1D : TabPrix) {
-                j = 1;
-                for (int val : tab1D) {
-                    if ( i == nbPersonnes && j == categorie) {
-                        if (val != 0){
-                            System.out.println("Cette chambre coute "+ val+ " euros");
-                        } else {
-                            System.out.println("Cette chambre n'est pas disponible !!");
-                        }
-                    }
-                    j++;
-                }
-                i++;
-            }
-        }
-
-        public void AffichDescription() {
-            System.out.println("Quelle catégorie de chambre voulez-vous ?");
-            categorie = sc.nextInt();
-            int j = 1;
-            for (int[] tab1D : TabPrix) {
-                j = 1;
-                for (int val : tab1D) {
-                    if ( j == categorie) {
-                        if (val != 0) {
-                            System.out.println("Cette chambre possède : " + TabDescript[j - 1]);
-                        }
-                    }
-                    j++;
-                }
+    public void AffichDescription(String[] td) {
+            System.out.println("La chambre possède : "+td[categorie -1]);
 
             }
         }
-}
+
--- Gestion_Hotel/src/Main.java
+++ Gestion_Hotel/src/Main.java
@@ -1,84 +1,26 @@
 import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+//         tableau des prix des chambres : tabPrix
+        int tp[][]  = new int[4][6] ;
+        tp[0][0] = 100; tp[0][1]=120 ; tp[0][2] = 130; tp[0][3]=150; tp[0][4] = 0;  tp[0][5]=0;
+        tp[1][0] = 130; tp[1][1]=160 ; tp[1][2] = 170; tp[1][3]=190; tp[1][4] = 0;  tp[1][5]=0;
+        tp[2][0] = 170; tp[2][1]=200 ; tp[2][2] = 210; tp[2][3]=230; tp[2][4] = 270;  tp[2][5]=350;
+        tp[3][0] = 200; tp[3][1]=230 ; tp[3][2] = 240; tp[3][3]=260; tp[3][4] = 300;  tp[3][5]=400;
 
-    public static void main(String args[]) {
+        // tableau des descriptions des chambres : tabDescript
+        String td [ ] = {"Lavabo","WC, television","Cabine douche, television","WC, cabine douche, Television","WC, Salle de bain + douche, Television","2 pièces, WC, Salle de bain + douche, Television" } ;
 
-        // déclarer et initialiser un tableau 2D
-        int[][] TabPrix = {
-                {100, 120, 130, 150, 0, 0},
-                {130, 160, 170, 190, 0, 0},
-                {170, 200, 210 ,230 ,270 , 350},
-                {200, 230, 240, 260,300,400},
-        };
+        Chambre c = new Chambre(1,2,1);
+        System.out.println("Categorie : "+c.DonnerCateg());
+        System.out.println("Capacite : "+c.DonnerNbPers());
+        System.out.println("Prix : "+c.DonnerPrix(tp));
+        c.AffichDescription(td);
 
-        // afficher le tableau 2D
-        for (int[] tab: TabPrix) {
-            for (int s: tab) {
-                System.out.print(s + "\t");
-            }
-            System.out.println("\n");
-        }
-
-//        int j, i = 1;
-//        for (int[] tab1D : TabPrix) {
-//            j = 1;
-//            for (int val : tab1D) {
-//                System.out.println("[" + i + "][" + j + "] = " + val);
-//                j++;
-//            }
-//            i++;
-//        }
-
-        String[] TabDescript = {
-                "Lavabo", "WC, Télévision", "Cabine Douche, Télévision", "WC , cabine douche, télévision" ,
-                "wc, salle de bain, douche, télévision", "2 pièces, salle de bain, douche, wc, télévision"
-        };
-
-            int nbPerson;
-            int categorie;
-        Scanner sc = new Scanner(System.in);
-
-        Chambre test = new Chambre(1,1,1);
-        test.AffichDescription();
-
-//        System.out.println("A combien êtes-vous ? 4 pers. max.");
-//        nbPerson = sc.nextInt();
-//
-//        while (nbPerson > 4 || nbPerson < 1) {
-//            System.out.println("Apprend a lire, 4 pers. max. et 1 pers. minimum");
-//            System.out.println("A combien êtes-vous ? 4 pers. max.");
-//            nbPerson = sc.nextInt();
-//        }
-//
-//        System.out.println("Quelle catégorie de chambre voulez-vous ?");
-//        categorie = sc.nextInt();
-//
-//        while (categorie > 6 || categorie < 1) {
-//            System.out.println("Cette catégorie n'existe pas");
-//            System.out.println("Quelle catégorie de chambre voulez-vous ?");
-//            categorie = sc.nextInt();
-//        }
-
-//        int j, i = 1;
-//        for (int[] tab1D : TabPrix) {
-//            j = 1;
-//            for (int val : tab1D) {
-//                if ( i == nbPerson && j == categorie) {
-//                    if (val != 0){
-//                        System.out.println("Une chambre est disponible");
-//                        System.out.println("Cette chambre coute "+ val+ " euros");
-//                        System.out.println("Cette chambre possède : "+TabDescript[j-1]);
-//                    } else {
-//                        System.out.println("Cette chambre n'est pas disponible !!");
-//                    }
-//                }
-//                j++;
-//            }
-//            i++;
-//        }
-//        Chambre test = new Chambre(25, 4, 2);
-//        System.out.println("Quelle chambre voulez vous prendre (numéro) ?");
-//        test.donnerNumero();
+        Hotel h = new Hotel();
+        h.AffichCh(1,tp,td);
     }
+
+
 }
